7ad3d9ad8ceb4e31e980d08d6f626aaeba7c5d6a
Nick <oliinyk_mykola@inbox.ru>
2017-04-20 10:20:44 +0300

    external links added

--- app/src/main/java/link/nick/com/moviedb/Const.java
+++ app/src/main/java/link/nick/com/moviedb/Const.java
@@ -7,4 +7,6 @@ package link.nick.com.moviedb;
 public class Const {
 
     public static String v3auth_key = "99602a08ae69e3b5e73f8e69ca38d3a1";
-}
+    public static String link1 = "https://habrahabr.ru/post/305478/";
+    public static String link2 = "http://www.androidhive.info/2016/05/android-working-with-retrofit-http-library/";
+ }
